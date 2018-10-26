678651242e1d83fffff259fd74825ece53df9702
MatheusBorgesKamla <matheuskamla@usp.br>
2018-10-25 21:22:08 -0300

    Adicionei o teste a partir dos parametros argc e argv para reconhecer a funcionalidade 1 e a funçã para gerar o primeiro campo

--- Code.c
+++ Code.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
 
 int main(int argc, char const *argv[])
 {
-
+    
+    if(!strcmp(argv[1],"1") && (argc==4)){
+        //Funcionalidade 1 :
+        int n = atoi(argv[3]);
+        int* campo1 = (int*) malloc(n*sizeof(int));
+        Gera_Campo1(n,campo1);
+    }else{
+        printf("Erro na pasagem de parâmetros\n");
+    }
     return 0;
 }
+
+void Gera_Campo1(int n, int* vetor){
+    int i;
+    for(i=0;i<n;i++){
+        vetor[i] = rand()%50000;
+    }
+}
+
+
