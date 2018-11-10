f6421a111269b0339eda56c8ea9f1aa42dd3836e
MatheusBorgesKamla <matheuskamla@usp.br>
2018-11-10 12:44:59 -0200

    Salvo errado!

--- Code.c
+++ Code.c
@@ -5,21 +5,21 @@
 int main(int argc, char const *argv[])
 {
 
-    if (!strcmp(argv[1], "1") && (argc == 4))
-    {
+    if (!strcmp(argv[1], "1") && (argc == 4)){
         //Funcionalidade 1 :
         int n = atoi(argv[3]);
         int *campo1 = (int *)malloc(n * sizeof(int));
         Gera_Campo1(n, campo1);
+        /*for(int i=0;i<n;i++){
+            printf("%d ",campo1[i]);
+        }*/
         char **campo2 = (char **)malloc(n * sizeof(char *));
         for (int i = 0; i < n; i++)
         {
             campo2[i] = (char *)malloc(30 * sizeof(char));
         }
-    }
-    else
-    {
-        printf("Erro na pasagem de parâmetros\n");
+    }else{
+        printf("Erro na passagem de parâmetros\n");
     }
     return 0;
 }
@@ -38,7 +38,7 @@ void Gera_Campo1(int n, int *vetor)
         }
         vetor[i] = aux[indice];
         aux[indice] = -1;
-    }  
+    }
     indice = rand()%50000;
     while(aux[indice]==(-1)){
         indice = rand()%50000;
@@ -46,13 +46,11 @@ void Gera_Campo1(int n, int *vetor)
     for(i=n*0.7;i<n;i++){
         vetor[i] = aux[indice];
     }
-    /*for(i=0;i<n;i++){
-        printf("%d ",vetor[i]);
-    }*/
+
 }
 
 void Gera_Campo2(int n, char *vetor[])
 {
     /*aux[100][30] = {"GOD", "WAR", "SNIPER", "FIGTH", "BAD", "WEAPON", "FIRE", "ARMY", "ADVANCED", "GHOST", "CHICKEN", "GROW", "HIGH", "MAGIC", "WORLD", "ROCK", "BATTLE", "SNOW", "GAME", "YELLOW", "CALL", "BLACK", "OPS", "HACKER", "SOLDIER", "ZOMBIE", "DRAGONS", "SHIELD", "COLOSSUS", "SHADOW", "DIRTY", "TITANS", "BREAKING", "CRAZY", "NIGTH", "MUTANTS",
-    "HERO", "SPEED", "DUTY", "WORLD", "NAME", "TRIBAL", "CIVILIZATION", "ALIEN", "PREDATOR", "BOOK", "WORD", "PRAY", "CHURCH", "KNIFE", "SWORD", "AXE", "STRONG", "POTTER", "DIVISION", "ROYALE", "UNKNOW", "BATTLEGROUND", "BATTLEFIELD", "" };*/
+    "HERO", "SPEED", "" };*/
 }
